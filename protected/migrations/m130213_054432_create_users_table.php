247be8b325c31b348a028f2ce65b15a88d5c77a2
almir <almir@debian.(none)>
2013-02-13 00:46:54 -0500

    add migrations

---
+++ protected/migrations/m130213_054432_create_users_table.php
@@ -0,0 +1,25 @@
+<?php
+
+class m130213_054432_create_users_table extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m130213_054432_create_users_table does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
